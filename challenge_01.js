058a7a5a5dc416f13b846670741a5f68de1aeaa5
Tinneff <tadeus@unitybox.de>
2023-01-16 10:44:17 +0100

    variante 1 ++

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,10 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+let firstNumber, secondNumbere;
+firstNumber = prompt("Bitte Zahl eingeben"); 
+secondNumber = prompt("Bitte zweite Zahl eingeben");
+summe = (Number(firstNumber) + Number(secondNumber));
+
+console.log(summe);
